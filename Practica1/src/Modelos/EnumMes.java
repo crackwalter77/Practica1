ab45b4f935b9fe60a713cf4b163e0dc1c33f81a5
Walter Yaguana <104579317+crackwalter77@users.noreply.github.com>
2023-05-12 10:43:38 -0500

    Primer Commit
    Avance

---
+++ Practica1/src/Modelos/EnumMes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author walter
+ */
+public enum EnumMes {
+    ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE;
+}
